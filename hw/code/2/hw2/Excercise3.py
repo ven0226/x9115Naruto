c46123be522c3a5aabab92baa61f8b3313f0c596
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-09-01 15:50:50 -0400

    Exercise 3

---
+++ hw/code/2/hw2/Excercise3.py
@@ -0,0 +1,36 @@
+def right_justify(s):
+    rwidth = 70
+    print s.rjust(rwidth)
+
+def do_twice(f,val):
+    f(val)
+    f(val)
+
+def do_four(f,val):
+    do_twice(f,val)
+    do_twice(f,val)
+
+def print_spam(val):
+    print val
+
+def grid(rows,cols):
+    for i in range(0,rows):
+        print "+",
+        for k in range(0,cols):
+            for j in range(1,5):
+                print "-",
+            print "+",
+        print
+        for a in range(1,5):
+            print "/",
+            for k in range(0,cols):
+                for j in range(1,5):
+                    print " ",
+                print "/",
+            print
+    print "+",
+    for k in range(0,cols):
+        for j in range(1,5):
+            print "-",
+        print "+",
+    print
