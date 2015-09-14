6a96c61564ab5c6ffb8f98051d29252fc6386092
Venkatesh Sambandamoorthy <ven0226@gmail.com>
2015-09-14 18:20:48 -0400

    poker

--- hw/code/3/PokerHand.py
+++ hw/code/3/PokerHand.py
@@ -21,6 +21,48 @@ class PokerHand(Hand):
         for card in self.cards:
             self.suits[card.suit] = self.suits.get(card.suit, 0) + 1
 
+    def suit_rank(self):
+        """Builds a histogram of the suits that appear in the hand.
+
+        Stores the result in attribute suits.
+        """
+        self.suits = {}
+        for card in self.cards:
+            self.suits[card.rank] = self.suits.get(card.rank, 0) + 1
+
+    
+    def has_pair(self):
+        self.suit_rank()
+        for val in self.suits.values():
+            if val >= 2:
+                return True
+        return False
+    
+    def has_two_pair(self):
+        self.suit_rank()
+        count = 0
+        for val in self.suits.values():
+            if val >= 2:
+                count += 1
+
+        if count >= 2:
+            return True
+        return False
+    
+    def has_three_kind(self):
+        self.suit_rank()
+        count = 0
+        for val in self.suits.values():
+            if val >= 2:
+                count += 1
+
+        if count >= 3:
+            return True
+        return False
+    
+    def has_straight(self):
+        return False
+        
     def has_flush(self):
         """Returns True if the hand has a flush, False otherwise.
       
@@ -30,6 +72,23 @@ class PokerHand(Hand):
         for val in self.suits.values():
             if val >= 5:
                 return True
+        return False
+    
+    def has_full_house(self):
+        return False
+        
+    def has_four_kind(self):
+        self.suit_rank()
+        count = 0
+        for val in self.suits.values():
+            if val >= 2:
+                count += 1
+
+        if count >= 4:
+            return True
+        return False
+        
+    def has_straight_flush(self):
         return False
 
 
@@ -44,5 +103,5 @@ if __name__ == '__main__':
         deck.move_cards(hand, 7)
         hand.sort()
         print hand
-        print hand.has_flush()
+        print hand.has_two_pair()
         print ''
