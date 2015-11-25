dfad9cf0cb3821d17b3b14712259d75b177d60ba
ven0226 <ven0226@gmail.com>
2015-11-24 21:30:48 -0500

    adding type 2

--- hw/code/890/code8/optimizers/sa.py
+++ hw/code/890/code8/optimizers/sa.py
@@ -13,26 +13,36 @@ def sa(mod):
     k = 1.0
     kmax = 999.0
     Utility.say("1|")
+    cur = []
     while k < kmax:
       sn = mod.generate()
       norm, en = mod.get_energy(sn,norm)
       if en < eb:
-           sb = sn[:]
-           eb = en
-           Utility.say("!")
+        sb = sn[:]
+        eb = en
+        Utility.say("!")
+        cur.append("!")
       if en < e:
-          s = sn[:]
-          e = en
-          Utility.say("+")
+        s = sn[:]
+        e = en
+        Utility.say("+")
+        cur.append("+")
       elif mod.prob(e, en, k*7/(kmax)) < random.random():
-          s = sn
-          e = en
-          Utility.say("?")
+        s = sn
+        e = en
+        Utility.say("?")
+        cur.append("?")
       Utility.say(".")
+      cur.append(".")
 
       k = k + 1
       if k % 25 == 0:
-          Utility.say("\n"+str(int(k))+"|")
+        if not cur.__contains__("!") and not cur.__contains__("+"):
+            break;
+        cur = []
+        Utility.say("\n"+str(int(k))+"|")
+
+
 
     f1,f2 = mod.objs(sb)
     Utility.printOutput('Success',f1,f2,sb,f1+f2)
